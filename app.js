1fb36509ce808a0ba7ab1c93460a9b3c0abab4be
Phuc <duyphuclenguyen@gmail.com>
2025-02-26 12:56:02 -0500

    Add test and run

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 
-// get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // You might want to replace this with a logger since no-console is enabled
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
